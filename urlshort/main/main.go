14965f24d21f262567a1424e9007d17738cb33e7
y2k-shubham <y2k.shubhamgupta@gmail.com>
2020-03-29 00:55:49 +0530

    [refactor] (urlshort) Break main method into smaller functions

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -9,23 +9,7 @@ import (
 )
 
 func main() {
-	mux := defaultMux()
-
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
-
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
+	mapHandler := BuildDefaultHandler()
 
 	var pathRulesString string
 	yamlFileName := flag.String("yaml-file", "", "YAML input file containing path rules")
@@ -34,16 +18,38 @@ func main() {
 	if *yamlFileName != "" {
 		pathRulesString = urlshort.ReadYamlFile(*yamlFileName)
 	} else {
-		pathRulesString = yaml
+		pathRulesString = GetDefaultYamlRules()
 	}
 
 	yamlHandler, err := urlshort.YAMLHandler([]byte(pathRulesString), mapHandler)
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Println("Starting the server on :8080")
 	//http.ListenAndServe(":8080", mapHandler)
 	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func GetDefaultYamlRules() string {
+	return `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+}
+
+func BuildDefaultHandler() http.HandlerFunc {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	return mapHandler
 }
 
 func defaultMux() *http.ServeMux {
