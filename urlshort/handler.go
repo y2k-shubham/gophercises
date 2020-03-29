71c1ad4a929594e8eec9f7083e5692591e09a95f
y2k-shubham <y2k.shubhamgupta@gmail.com>
2020-03-29 11:06:21 +0530

    [refactor] (urlshort) Refactor code

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,11 +1,33 @@
 package urlshort
 
 import (
-	"encoding/json"
+	"fmt"
 	"github.com/go-yaml/yaml"
 	"log"
 	"net/http"
 )
+
+func BuildDefaultHandler() http.HandlerFunc {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := MapHandler(pathsToUrls, mux)
+	return mapHandler
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -23,7 +45,7 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
+// UnmarshalHandler will parse the provided YAML and then return
 // an http.HandlerFunc (which also implements http.Handler)
 // that will attempt to map any paths to their corresponding
 // URL. If the path is not provided in the YAML, then the
@@ -39,33 +61,29 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	var m []map[interface{}]interface{}
-
-	err := yaml.Unmarshal(yml, &m)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
+func UnmarshalHandler(bytes []byte, isYml bool, fallback http.Handler) (http.HandlerFunc, error) {
 	pathMap := make(map[string]string)
-	for _, myMap := range m {
-		pathMap[myMap["path"].(string)] = myMap["url"].(string)
-	}
+	var err error
 
-	return MapHandler(pathMap, fallback), err
-}
-
-func JSONHandler(jsn []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	var m []map[string]interface{}
-
-	err := json.Unmarshal(jsn, &m)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	pathMap := make(map[string]string)
-	for _, myMap := range m {
-		pathMap[myMap["path"].(string)] = myMap["url"].(string)
+	// had to duplicate code just because type in list of maps 'm' differs for yaml & json unmarshal methods
+	if isYml {
+		var m []map[interface{}]interface{}
+		err = yaml.Unmarshal(bytes, &m)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		for _, myMap := range m {
+			pathMap[myMap["path"].(string)] = myMap["url"].(string)
+		}
+	} else {
+		var m []map[string]interface{}
+		err = yaml.Unmarshal(bytes, &m)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		for _, myMap := range m {
+			pathMap[myMap["path"].(string)] = myMap["url"].(string)
+		}
 	}
 
 	return MapHandler(pathMap, fallback), err
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -9,29 +9,10 @@ import (
 )
 
 func main() {
-	mapHandler := BuildDefaultHandler()
+	mapHandler := urlshort.BuildDefaultHandler()
 
-	var pathRulesString string
-	// go run urlshort/main/main.go -yaml-file my-rules.yml
-	yamlFileName := flag.String("yaml-file", "", "YAML file containing path rules")
-	// go run urlshort/main/main.go -json-file my-rules.json
-	jsonFileName := flag.String("json-file", "", "JSON file containing path rules")
-
-	flag.Parse()
-
-	var err error
-	var usedHandler http.HandlerFunc
-	if *jsonFileName != "" {
-		pathRulesString = urlshort.ReadFile(*jsonFileName)
-		usedHandler, err = urlshort.JSONHandler([]byte(pathRulesString), mapHandler)
-	} else {
-		if *yamlFileName != "" {
-			pathRulesString = urlshort.ReadFile(*yamlFileName)
-		} else {
-			pathRulesString = GetDefaultYamlRules()
-		}
-		usedHandler, err = urlshort.YAMLHandler([]byte(pathRulesString), mapHandler)
-	}
+	pathRulesString, isYml := GetPathRules()
+	usedHandler, err := urlshort.UnmarshalHandler([]byte(pathRulesString), isYml, mapHandler)
 	if err != nil {
 		panic(err)
 	}
@@ -41,6 +22,31 @@ func main() {
 	http.ListenAndServe(":8080", usedHandler)
 }
 
+func GetPathRules() (string, bool) {
+	var pathRulesString string
+	var isYml bool
+	// go run urlshort/main/main.go -yaml-file my-rules.yml
+	yamlFileName := flag.String("yaml-file", "", "YAML file containing path rules")
+	// go run urlshort/main/main.go -json-file my-rules.json
+	jsonFileName := flag.String("json-file", "", "JSON file containing path rules")
+
+	flag.Parse()
+
+	if *jsonFileName != "" {
+		pathRulesString = urlshort.ReadFile(*jsonFileName)
+		isYml = false
+	} else {
+		isYml = true
+		if *yamlFileName != "" {
+			pathRulesString = urlshort.ReadFile(*yamlFileName)
+		} else {
+			pathRulesString = GetDefaultYamlRules()
+		}
+	}
+
+	return pathRulesString, isYml
+}
+
 func GetDefaultYamlRules() string {
 	return `
 - path: /urlshort
@@ -48,26 +54,4 @@ func GetDefaultYamlRules() string {
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
 `
-}
-
-func BuildDefaultHandler() http.HandlerFunc {
-	mux := defaultMux()
-
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
-	return mapHandler
-}
-
-func defaultMux() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-	return mux
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
 }
